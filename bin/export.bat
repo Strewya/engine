1d569eb717c476e8eb860d87c886f0329db85443
Marko Pipal <mp42705@gmail.com>
2014-03-11 16:10:52 +0100

    Made a custom build step for pkg files.

--- bin/export.bat
+++ bin/export.bat
@@ -2,4 +2,4 @@ set pth=%~dp0
 
 cl.exe /EP %pth%..\src\Games\luaExport.cpp > %pth%..\src\Games\luaExport.pkg
 
-%pth%tolua++.exe -n core -H %pth%..\src\Scripting\luaBinding.h -o %pth%..\src\Scripting\luaBinding.cpp %pth%..\src\Scripting\luaExport.pkg
+%pth%tolua++.exe -n core -H %pth%..\src\Scripting\luaBinding.h -o %pth%..\src\Scripting\luaBinding.cpp %pth%..\src\luaExportMaster.pkg
